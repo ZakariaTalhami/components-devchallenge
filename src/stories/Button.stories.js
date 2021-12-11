26fc9bac6eeecd0d8dc571349afd10bc496946fd
ZakariaTalhami <zakaria.a.a.talhami@gmail.com>
2021-12-11 22:00:27 +0200

    Add Button stories for color, size and variant

---
+++ src/stories/Button.stories.js
@@ -0,0 +1,64 @@
+import Button from '../components/button/Button';
+
+export default {
+  title: 'Button',
+  component: Button,
+  argTypes: { onClick: { action: 'onClick' } },
+};
+
+const template = ({ label, ...args }) => <Button {...args} children={label} />;
+
+export const DefaultButton = template.bind({});
+DefaultButton.args = {
+  label: 'Default Button',
+  shadowed: true,
+};
+
+export const PrimaryButton = template.bind({});
+PrimaryButton.args = {
+  label: 'Primary Button',
+  color: 'primary',
+  shadowed: true,
+};
+
+export const SecondaryButton = template.bind({});
+SecondaryButton.args = {
+  label: 'Secondary Button',
+  color: 'secondary',
+  shadowed: true,
+};
+
+export const DangerButton = template.bind({});
+DangerButton.args = {
+  label: 'Danger Button',
+  color: 'danger',
+  shadowed: true,
+};
+
+export const OutlineButton = template.bind({});
+OutlineButton.args = {
+  label: 'Outline Button',
+  color: 'primary',
+  variant: 'outline',
+};
+
+export const TextButton = template.bind({});
+TextButton.args = {
+  label: 'Text Button',
+  color: 'primary',
+  variant: 'text',
+};
+
+export const SmallButton = template.bind({});
+SmallButton.args = {
+  label: 'Small Button',
+  color: 'primary',
+  size: 'sm',
+};
+
+export const LargeButton = template.bind({});
+LargeButton.args = {
+  label: 'Large Button',
+  color: 'primary',
+  size: 'lg',
+};
